f833a45b9970ec6c0f6688524950277a49edd0b8
Peter Watkins <watkipet@gmail.com>
2017-01-05 15:57:34 -0800

    Simplify namespaces

--- ElfParser/Program.cs
+++ ElfParser/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
-using DWARFSharp;
+using DWARFSharp.Dwarf;
 using ELFSharp.ELF;
 
 namespace ElfParser
@@ -93,8 +93,8 @@ DESCRIPTION
             }
 
             // Load ELF file and extract .debug_str
-            var elfFile = ELFSharp.ELF.ELFReader.Load(elfName);
-			EBitConverter.DataIsLittleEndian = elfFile.Endianess == Endianess.LittleEndian;
+            var elfFile = ELFReader.Load(elfName);
+			DWARFSharp.EBitConverter.DataIsLittleEndian = elfFile.Endianess == Endianess.LittleEndian;
             var strData = elfFile.Sections.Where(s => s.Name == stringsSectioName).First().GetContents().ToList();
 
             // Parse abbreviations and compilation units
@@ -112,8 +112,8 @@ DESCRIPTION
 
                 // Extract all variables with location attribute
                 var dieList = cu.GetChildren().Where(d =>
-                    d.Tag == DWARFSharp.Dwarf.DW_TAG.Variable && 
-                    d.AttributeList.Exists(a => a.Name == DWARFSharp.Dwarf.DW_AT.Location));
+                    d.Tag == DW_TAG.Variable && 
+                    d.AttributeList.Exists(a => a.Name == DW_AT.Location));
 
                 // Iterate through all variables
                 foreach (var die in dieList)
@@ -121,8 +121,8 @@ DESCRIPTION
                     // Initialize variable
                     var name = die.GetName(strData);
                     var variable = new Variable(name);
-                    var locAttr = die.AttributeList.Find(a => a.Name == DWARFSharp.Dwarf.DW_AT.Location);
-                    variable.Address = EBitConverter.ToInt32(locAttr.Value.Skip(1).Take(4).ToArray(), 0);
+                    var locAttr = die.AttributeList.Find(a => a.Name == DW_AT.Location);
+                    variable.Address = DWARFSharp.EBitConverter.ToInt32(locAttr.Value.Skip(1).Take(4).ToArray(), 0);
 
                     // Traverse variable tree
                     TraverseVariableRecursive(cu, die, variable, strData, false);
@@ -152,20 +152,20 @@ DESCRIPTION
             #endregion
         }
         // Traverse variable tree
-        static void TraverseVariableRecursive(DWARFSharp.Dwarf.CompilationUnit cu, DWARFSharp.Dwarf.DebuggingInformationEntry die, Variable varTree, List<byte> strData, bool isPointer)
+        static void TraverseVariableRecursive(CompilationUnit cu, DebuggingInformationEntry die, Variable varTree, List<byte> strData, bool isPointer)
         {
             var dieList = DeflateDieListRecursive(cu.GetChildren());
-            DWARFSharp.Dwarf.DebuggingInformationEntry typeDie;
+            DebuggingInformationEntry typeDie;
             DWARFSharp.Dwarf.Attribute sizeDie;
             Variable memberVar;
 
             switch (die.Tag)
             {
-                case DWARFSharp.Dwarf.DW_TAG.EnumerationType:
+                case DW_TAG.EnumerationType:
                     // Add size to variable
-                    sizeDie = die.AttributeList.Find(a => a.Name == DWARFSharp.Dwarf.DW_AT.ByteSize);
+                    sizeDie = die.AttributeList.Find(a => a.Name == DW_AT.ByteSize);
                     // TODO: Should we be casting? Is sizeDie.Value really 8 bytes?
-                    varTree.ByteSize = (int) EBitConverter.ToUInt64(sizeDie.Value, 0);
+                    varTree.ByteSize = (int)DWARFSharp.EBitConverter.ToUInt64(sizeDie.Value, 0);
 
                     switch (varTree.ByteSize)
                     {
@@ -183,12 +183,12 @@ DESCRIPTION
                             break;
                     }
                     break;
-                case DWARFSharp.Dwarf.DW_TAG.BaseType:
+                case DW_TAG.BaseType:
                     // Add size to variable
-                    sizeDie = die.AttributeList.Find(a => a.Name == DWARFSharp.Dwarf.DW_AT.ByteSize);
+                    sizeDie = die.AttributeList.Find(a => a.Name == DW_AT.ByteSize);
                     varTree.ByteSize = sizeDie.Value[0];
                     break; // Reached bottom of tree
-                case DWARFSharp.Dwarf.DW_TAG.Typedef:
+                case DW_TAG.Typedef:
                     // Pointers aren't fetchable
                     if (!isPointer)
                     {
@@ -207,9 +207,9 @@ DESCRIPTION
                         varTree.ByteSize = 4;
                     }
                     break;
-                case DWARFSharp.Dwarf.DW_TAG.UnionType:
+                case DW_TAG.UnionType:
                     // Add size to variable
-                    sizeDie = die.AttributeList.Find(a => a.Name == DWARFSharp.Dwarf.DW_AT.ByteSize);
+                    sizeDie = die.AttributeList.Find(a => a.Name == DW_AT.ByteSize);
                     varTree.ByteSize = sizeDie.Value[0];
                     
                     // Traverse only first member
@@ -223,12 +223,12 @@ DESCRIPTION
                     typeDie = dieList.Find(d => d.Id == m.GetTypeId());
                     TraverseVariableRecursive(cu, m, memberVar, strData, isPointer);
                     break;
-                case DWARFSharp.Dwarf.DW_TAG.StructureType:
+                case DW_TAG.StructureType:
                     // Add size to variable
                     var size = new byte[8];
-                    die.AttributeList.Find(a => a.Name == DWARFSharp.Dwarf.DW_AT.ByteSize).Value.CopyTo(size, 0);
+                    die.AttributeList.Find(a => a.Name == DW_AT.ByteSize).Value.CopyTo(size, 0);
                     // TODO: Should we be casting?
-                    varTree.ByteSize = (int) EBitConverter.ToInt64(size, 0);
+                    varTree.ByteSize = (int)DWARFSharp.EBitConverter.ToInt64(size, 0);
 
                     // Traverse each member
                     foreach (var member in die.Children)
@@ -242,7 +242,7 @@ DESCRIPTION
                         TraverseVariableRecursive(cu, member, memberVar, strData, isPointer);
                     }
                     break;
-                case DWARFSharp.Dwarf.DW_TAG.ArrayType:
+                case DW_TAG.ArrayType:
                     DWARFSharp.Dwarf.Attribute arrayAttr;
                     // TODO: Array sizes are stored in UData attributes. These are LEB128 and 
                     // assumed to hold up to 8 bytes. However, are array sizes only a max of 4 
@@ -252,29 +252,29 @@ DESCRIPTION
                     switch(die.Children.Count)
                     {
                         case 1:
-                            arrayAttr = die.Children.ElementAt(0).AttributeList.Find(a => a.Name == DWARFSharp.Dwarf.DW_AT.UpperBound);
+                            arrayAttr = die.Children.ElementAt(0).AttributeList.Find(a => a.Name == DW_AT.UpperBound);
 
                             // Add array size to variable
                             if (arrayAttr != null)
                             {
                                 arrayAttr.Value.CopyTo(arraySize, 0);
-                                varTree.ArraySize[0] = (int) (EBitConverter.ToInt64(arraySize, 0) + 1);
+                                varTree.ArraySize[0] = (int)(DWARFSharp.EBitConverter.ToInt64(arraySize, 0) + 1);
                             }
                             break;
                         case 2:
-                            arrayAttr = die.Children.ElementAt(0).AttributeList.Find(a => a.Name == DWARFSharp.Dwarf.DW_AT.UpperBound);
+                            arrayAttr = die.Children.ElementAt(0).AttributeList.Find(a => a.Name == DW_AT.UpperBound);
 
                             // Add array size to variable
                             if (arrayAttr != null)
                             {
                                 varTree.ArraySize[0] = arrayAttr.Value[0] + 1;
 
-                                arrayAttr = die.Children.ElementAt(1).AttributeList.Find(a => a.Name == DWARFSharp.Dwarf.DW_AT.UpperBound);
+                                arrayAttr = die.Children.ElementAt(1).AttributeList.Find(a => a.Name == DW_AT.UpperBound);
 
                                 if (arrayAttr != null)
                                 {
                                     arrayAttr.Value.CopyTo(arraySize, 0);
-                                    varTree.ArraySize[1] = (int) (EBitConverter.ToInt64(arraySize, 0) + 1);
+                                    varTree.ArraySize[1] = (int)(DWARFSharp.EBitConverter.ToInt64(arraySize, 0) + 1);
                                 }
                             }
                             break;
@@ -287,9 +287,9 @@ DESCRIPTION
                     typeDie = dieList.Find(d => d.Id == die.GetTypeId());
                     TraverseVariableRecursive(cu, typeDie, varTree, strData, isPointer);
                     break;
-                case DWARFSharp.Dwarf.DW_TAG.Member:
+                case DW_TAG.Member:
                     // Add relative member address
-                    var locAttr = die.AttributeList.Find(a => a.Name == DWARFSharp.Dwarf.DW_AT.DataMemberLocation);
+                    var locAttr = die.AttributeList.Find(a => a.Name == DW_AT.DataMemberLocation);
                     if (locAttr != null)
                     {
                         var addr = new byte[4];
@@ -297,7 +297,7 @@ DESCRIPTION
 
                         // Skip first byte (0x23) and go to data
                         var idx = 1;
-                        varTree.Address = (int)DWARFSharp.Dwarf.LEB128.ReadUnsigned(locAttr.Value.ToList(), ref idx); 
+                        varTree.Address = (int)LEB128.ReadUnsigned(locAttr.Value.ToList(), ref idx); 
                     }
 
                     // Continue down tree
@@ -312,7 +312,7 @@ DESCRIPTION
                     if (typeDie != null)
                     {
                         // Make sure to flag pointers
-                        isPointer = die.Tag == DWARFSharp.Dwarf.DW_TAG.PointerType || isPointer;
+                        isPointer = die.Tag == DW_TAG.PointerType || isPointer;
 
                         // Don't recurse to a type that we're already inside
                         // TODO: Do this everywhere.
@@ -327,7 +327,7 @@ DESCRIPTION
             }
         }
 
-        static bool IdBelongsToParent(DWARFSharp.Dwarf.DebuggingInformationEntry die, int id)
+        static bool IdBelongsToParent(DebuggingInformationEntry die, int id)
         {
             if (die.Id == id)
             {
@@ -400,17 +400,17 @@ DESCRIPTION
         }
 
         // Read and parse abbreviations from ELF file
-        static List<DWARFSharp.Dwarf.Abbreviation> ExtractAbbrevList(IELF elfFile)
+        static List<Abbreviation> ExtractAbbrevList(IELF elfFile)
         {
-            var abbrevList = new List<DWARFSharp.Dwarf.Abbreviation>();
+            var abbrevList = new List<Abbreviation>();
             var abbrevData = elfFile.Sections.Where(s => s.Name == ".debug_abbrev").First().GetContents().ToList();
 
             var index = 0;
             while (index < abbrevData.Count)
             {
                 var startIndex = index;
-                DWARFSharp.Dwarf.Abbreviation abbrev;
-                while ((abbrev = DWARFSharp.Dwarf.Parse.Abbreviation(abbrevData, ref index, startIndex)) != null)
+                Abbreviation abbrev;
+                while ((abbrev = Parse.Abbreviation(abbrevData, ref index, startIndex)) != null)
                 {
                     abbrevList.Add(abbrev);
                 }
@@ -419,23 +419,23 @@ DESCRIPTION
         }
 
         // Read and parse for compilation units from ELF file
-        static List<DWARFSharp.Dwarf.CompilationUnit> ExtractCuList(IELF elfFile, List<DWARFSharp.Dwarf.Abbreviation> abbrevList)
+        static List<CompilationUnit> ExtractCuList(IELF elfFile, List<Abbreviation> abbrevList)
         {
-            var cuListFlat = new List<DWARFSharp.Dwarf.CompilationUnit>();
-            var cuList = new List<DWARFSharp.Dwarf.CompilationUnit>();
+            var cuListFlat = new List<CompilationUnit>();
+            var cuList = new List<CompilationUnit>();
             var index = 0;
 
             var infoData = elfFile.Sections.Where(s => s.Name == ".debug_info").First().GetContents().ToList();
 
-            DWARFSharp.Dwarf.CompilationUnit cu;
-            while ((cu = DWARFSharp.Dwarf.Parse.CU(infoData, ref index, abbrevList)) != null)
+            CompilationUnit cu;
+            while ((cu = Parse.CU(infoData, ref index, abbrevList)) != null)
                 cuListFlat.Add(cu);
 
             foreach (var c in cuListFlat)
             {
                 index = 0;
                 var dieList = InflateDieListRecursive(c.DieList, ref index);
-                var inflatedCu = new DWARFSharp.Dwarf.CompilationUnit(c.Cuh, dieList);
+                var inflatedCu = new CompilationUnit(c.Cuh, dieList);
                 cuList.Add(inflatedCu);
             }
 
@@ -443,16 +443,16 @@ DESCRIPTION
         }
         
         // Group children to parent DIEs
-        static List<DWARFSharp.Dwarf.DebuggingInformationEntry> InflateDieListRecursive(List<DWARFSharp.Dwarf.DebuggingInformationEntry> dieList, ref int index)
+        static List<DebuggingInformationEntry> InflateDieListRecursive(List<DebuggingInformationEntry> dieList, ref int index)
         {
-            var output = new List<DWARFSharp.Dwarf.DebuggingInformationEntry>();
+            var output = new List<DebuggingInformationEntry>();
             while (index < dieList.Count)
             {
                 var die = dieList.ElementAt(index);
                 index++;
                 if (die == null)
                     break;
-                if (die.HasChildren == DWARFSharp.Dwarf.DW_CHILDREN.Yes)
+                if (die.HasChildren == DW_CHILDREN.Yes)
                 {
                     var childDieList = InflateDieListRecursive(dieList, ref index);
                     die.AddDieList(childDieList);
@@ -467,15 +467,15 @@ DESCRIPTION
         }
 
         // Ungroup children to parent DIEs
-        static List<DWARFSharp.Dwarf.DebuggingInformationEntry> DeflateDieListRecursive(List<DWARFSharp.Dwarf.DebuggingInformationEntry> dieList)
+        static List<DebuggingInformationEntry> DeflateDieListRecursive(List<DebuggingInformationEntry> dieList)
         {
-            var output = new List<DWARFSharp.Dwarf.DebuggingInformationEntry>();
+            var output = new List<DebuggingInformationEntry>();
             for (var i = dieList.Count - 1; i >= 0; i--)
             {
                 var die = dieList.ElementAt(i);
                 output.Add(die);
 
-                if (die.HasChildren == DWARFSharp.Dwarf.DW_CHILDREN.Yes)
+                if (die.HasChildren == DW_CHILDREN.Yes)
                 {
                     var childDieList = DeflateDieListRecursive(die.Children);
                     output.AddRange(childDieList);
